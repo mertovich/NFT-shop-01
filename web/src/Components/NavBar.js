ea019ed47ae4fc68654fffd59af55204dba042fe
Periyot <mdsabinov@gmail.com>
2022-01-01 13:18:31 +0300

    fix(NavBar): Link router edit

--- web/src/Components/NavBar.js
+++ web/src/Components/NavBar.js
@@ -12,10 +12,10 @@ export default class NavBar extends Component {
                     <Container>
                         <Navbar.Brand href="#home"><img src={Logo} height='50px' width='50px' />  </Navbar.Brand>
                         <Nav className="me-right">
-                        <Link className='NavBarLink' to='/contact' >Home</Link>
-                        <Link className='NavBarLink' to='/contact' >Shop</Link>
-                        <Link className='NavBarLink' to='/contact' >Register</Link>
-                        <Link className='NavBarLink' to='/contact' >Login</Link>
+                        <Link className='NavBarLink' to='/' >Home</Link>
+                        <Link className='NavBarLink' to='/shop' >Shop</Link>
+                        <Link className='NavBarLink' to='/register' >Register</Link>
+                        <Link className='NavBarLink' to='/login' >Login</Link>
                         </Nav>
                     </Container>
                 </Navbar>
