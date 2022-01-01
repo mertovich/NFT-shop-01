bf0f7ae0b3907447237ae8d5ac80ae95f3768fe8
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-01 12:13:08 +0300

    add(NavBar)

---
+++ web/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './Pages/Home' 
+import NavBar from './Components/NavBar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{
+        background: '#121212',
+        height:'100vh',
+        width:'100%',
+        overflowX:'hidden',
+    }} >
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
+    )
+  }
+}
