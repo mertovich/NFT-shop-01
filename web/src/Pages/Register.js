052d8a8d5b34221cdefd7f85187f15b38ac297c8
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-01 14:30:56 +0300

    add(RegisterPage)

---
+++ web/src/Pages/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import RegisterForm from '../Components/RegisterForm'
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div>
+                <RegisterForm/>
+            </div>
+        )
+    }
+}
