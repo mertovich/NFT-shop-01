b1109f61ad080af89a471aa6af52c1af4bb14c55
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-01 14:32:33 +0300

    add(RegisterRouter)

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
-import {Route,Routes} from 'react-router-dom'
-import Home from './Pages/Home' 
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 import NavBar from './Components/NavBar'
+import Register from './Pages/Register'
 
 export default class App extends Component {
   render() {
     return (
       <div style={{
         background: '#121212',
-        height:'100vh',
-        width:'100%',
-        overflowX:'hidden',
-    }} >
-        <NavBar/>
+        height: '100vh',
+        width: '100%',
+        overflowX: 'hidden',
+      }} >
+        <NavBar />
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </div>
     )
