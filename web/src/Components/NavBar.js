3372acabe77ea107b824d7995ba5bf50403fa416
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-01 17:38:41 +0300

    fix(NavBar): Responsive

--- web/src/Components/NavBar.js
+++ web/src/Components/NavBar.js
@@ -10,16 +10,21 @@ export default class NavBar extends Component {
     render() {
         return (
             <div>
-                <Navbar className='NavBar'>
+                <Navbar collapseOnSelect expand="lg" variant="dark">
                     <Container>
-                        <Navbar.Brand href="#home"><img src={Logo} height='50px' width='50px' />  </Navbar.Brand>
-                        <Nav className="me-right">
-                            <Link className='NavBarLink' to='/' >Home</Link>
-                            <Link className='NavBarLink' to='/store' >Store</Link>
-                            <Link className='NavBarLink' to='/register' >Register</Link>
-                            <Link className='NavBarLink' to='/login' >Login</Link>
-                            {this.props.NavBarActive === 'true' ? <Link onMouseOut={() =>this.props.NavBarActiveButtonOver()} onMouseOver={()=> this.props.NavBarActiveButton()} className='NavBarLink' to='/basket' >Basket <img src={basketLogoRed} height='15px' width='15px' /> </Link> : <Link onMouseOut={() =>this.props.NavBarActiveButtonOver()} onMouseOver={()=> this.props.NavBarActiveButton()} className='NavBarLink' to='/basket' >Basket <img src={basketLogo} height='15px' width='15px' /> </Link>}
-                        </Nav>
+                        <Navbar.Brand href="#home"><img src={Logo} height='50px' width='50px' /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                            </Nav>
+                            <Nav>
+                                <Link className='NavBarLink' to='/' >Home</Link>
+                                <Link className='NavBarLink' to='/store' >Store</Link>
+                                <Link className='NavBarLink' to='/register' >Register</Link>
+                                <Link className='NavBarLink' to='/login' >Login</Link>
+                                {this.props.NavBarActive === 'true' ? <Link onMouseOut={() => this.props.NavBarActiveButtonOver()} onMouseOver={() => this.props.NavBarActiveButton()} className='NavBarLink' to='/basket' >Basket <img src={basketLogoRed} height='15px' width='15px' /> </Link> : <Link onMouseOut={() => this.props.NavBarActiveButtonOver()} onMouseOver={() => this.props.NavBarActiveButton()} className='NavBarLink' to='/basket' >Basket <img src={basketLogo} height='15px' width='15px' /> </Link>}
+                            </Nav>
+                        </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </div>
