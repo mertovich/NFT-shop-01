df5c9267ab67f136a03d5d9fc16fc2b30a15ec0b
Periyot <mdsabinov@gmail.com>
2022-01-02 11:28:08 +0300

    add(Productlist): POST method

---
+++ api/src/ProductModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+var productSchema = mongoose.Schema({
+    url:String,
+    name:String,
+    description:String,
+    artiste:String,
+    price:String,
+})
+
+var Product = mongoose.model('Product',productSchema)
+
+module.exports = Product
