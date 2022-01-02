f23b8354887839398c399c4d4786a10bc20f6b71
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-02 11:28:08 +0300

    add(Productlist): POST method

--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,36 @@
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser')
-require('dotenv').config();
+const Product = require('./ProductModel')
+const mongoose = require('mongoose')
 
-var urlEncodedParser = bodyParser.urlencoded
+mongoose.connect(process.env.MONGO_URL,(err)=>{
+    if (!err) {
+        console.log('db active')
+    }
+})
+
+var urlEncodedParser = bodyParser.urlencoded()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
+app.post('/productlist',urlEncodedParser,(req,res,next)=>{
+    let tmp = new Product({
+        url:req.body.url,
+        name:req.body.name,
+        description:req.body.description,
+        artiste:req.body.artiste,
+        price:req.body.price
+    })
+    tmp.save((err)=>{
+        if (err) {
+            throw err
+        }
+        res.status(200).end()
+    })
+})
 
 module.exports = app;
