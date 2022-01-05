b800ef436c67318e09d3619dde5570cf1fcc7cd4
Periyot <mdsabinov@gmail.com>
2022-01-05 15:53:04 +0300

    fix(typo): lower caser

--- web/src/Components/HomeOneSection.js
+++ web/src/Components/HomeOneSection.js
@@ -5,9 +5,9 @@ export default class HomeOneSection extends Component {
     render() {
         return (
             <div>
-                <h1 className='HomeOneSectionh1' >Next generation NFT Store</h1>
-                <p className='HomeOneSectionp1' >Easy and fast payment system with NFTs and btc valid on metaverse</p>
-                <button className='HomeOneSecitonButton'>Discover new NFTs</button>
+                <h1 className='HomeOneSectionh1' >Next Generation NFT Store</h1>
+                <p className='HomeOneSectionp1' >Easy and fast payment system with NFTs and BTC valid on Metaverse</p>
+                <button className='HomeOneSecitonButton'>Discover New NFTs</button>
             </div>
         )
     }
