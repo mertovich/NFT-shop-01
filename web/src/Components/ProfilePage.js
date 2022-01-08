19faae524fe9d86aff0b20f0f9e96b2deb2f247f
mertovich <88425310+mertovich@users.noreply.github.com>
2022-01-08 15:03:12 +0300

    add(ProfilePage): Balance btc icon

--- web/src/Components/ProfilePage.js
+++ web/src/Components/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Container,Button } from 'react-bootstrap'
 import '../App.css'
+import btcGif from '../images/btcGif.gif'
 
 export default class ProfilePage extends Component {
     render() {
@@ -12,7 +13,7 @@ export default class ProfilePage extends Component {
                     <p> name : {this.props.user.name} </p>
                     <p> Last Name : {this.props.user.lastName} </p>
                     <p> Email : {this.props.user.email} </p>
-                    <p> Balance : {this.props.user.balance} </p>
+                    <p> Balance : {this.props.user.balance} <img src={btcGif} height='25px' width='25px' /> </p>
                     <Button onClick={() => this.props.logOutButton()} variant="outline-danger">Log Out</Button>
                 </Container>
             </div>
